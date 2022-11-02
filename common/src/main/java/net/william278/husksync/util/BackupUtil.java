6118d2ad4051c4d1e2a15b9f5f948ab286ced235
FlyTeam <81072191+killerprojecte@users.noreply.github.com>
2022-11-02 13:56:44 +0800

    Commit

--- common/src/main/java/net/william278/husksync/util/BackupUtil.java
+++ common/src/main/java/net/william278/husksync/util/BackupUtil.java
@@ -4,9 +4,9 @@ import net.william278.husksync.HuskSync;
 import net.william278.husksync.data.DataSaveCause;
 import net.william278.husksync.player.OnlineUser;
 
-public class BackulUtil {
+public class BackupUtil {
     public static void handleAutoBackup(HuskSync plugin,OnlineUser user){
-       user.getUserData(plugin.getLoggingAdapter()).join().ifPresent(
+       user.getUserData(plugin.getLoggingAdapter(), plugin.getSettings()).join().ifPresent(
                 userData -> plugin.getDatabase().setUserData(user, userData, DataSaveCause.AUTO_BACKUP).join());
     }
 }
